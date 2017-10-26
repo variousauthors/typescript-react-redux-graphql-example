b6e77fde83748534d488100b2c30f0ecbabb4fae
various.authors <various.authors@gmail.com>
2017-10-25 17:24:16 -0700

    Create React App

---
+++ src/components/App.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        Example
+      </div>
+    );
+  }
+}
+
+export default App;
